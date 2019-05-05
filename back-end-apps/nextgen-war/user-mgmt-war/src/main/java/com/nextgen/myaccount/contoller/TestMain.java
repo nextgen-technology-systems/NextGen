78ca491ec2949561eb9cf1eb634d766d36d63735
Thippeswamy A H <thalappa@tracfone.com>
2019-05-05 12:20:00 +0530

    added changes to POM

---
+++ back-end-apps/nextgen-war/user-mgmt-war/src/main/java/com/nextgen/myaccount/contoller/TestMain.java
@@ -0,0 +1,15 @@
+package com.nextgen.myaccount.contoller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMain {
+
+	  private static final Logger logger 
+      = LoggerFactory.getLogger(TestMain.class);
+ 
+    public static void main(String[] args) {
+        logger.info("Example log from {}", TestMain.class.getSimpleName());
+    }
+
+}
